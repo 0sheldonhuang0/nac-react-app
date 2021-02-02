5943c68eb81a4913d658c9e32d72f3c78c615cd2
0sheldonhuang0 <33676083+0sheldonhuang0@users.noreply.github.com>
2021-02-02 20:26:11 +0100

    Update listItems.js

--- src/Components/listItems.js
+++ src/Components/listItems.js
@@ -1,22 +1,23 @@
-import React,{useState} from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import Collapse from '@material-ui/core/Collapse';
+import Collapse from "@material-ui/core/Collapse";
 import BarChartIcon from "@material-ui/icons/BarChart";
 import LayersIcon from "@material-ui/icons/Layers";
 import AssignmentIcon from "@material-ui/icons/Assignment";
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
-import DesktopWindowsIcon from '@material-ui/icons/DesktopWindows';
-import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
-import PetsIcon from '@material-ui/icons/Pets';
-import HomeIcon from '@material-ui/icons/Home';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import StarBorder from "@material-ui/icons/StarBorder";
+import DesktopWindowsIcon from "@material-ui/icons/DesktopWindows";
+import PhoneIphoneIcon from "@material-ui/icons/PhoneIphone";
+import PetsIcon from "@material-ui/icons/Pets";
+import HomeIcon from "@material-ui/icons/Home";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+import InfoIcon from "@material-ui/icons/Info";
 
 const useStyles = makeStyles((theme) => ({
   nested: {
@@ -32,50 +33,65 @@ export default function MainListItems() {
     setOpen(!open);
   };
   return (
-  <div>
-        <ListItem button component="a" href="/">
-      <ListItemIcon>
-        <HomeIcon />
-      </ListItemIcon>
-      <ListItemText primary="Accueil" />
-    </ListItem>
-    <ListItem button component="a" href="/espace">
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Mon espace" />
-    </ListItem>
-    <ListItem button component="a" href="/user-guide">
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Mode d'emploi" />
-    </ListItem>
-    <ListItem button  onClick={handleClick}>
-      <ListItemIcon>
-        <LibraryBooksIcon />
-      </ListItemIcon>
-      <ListItemText
-        primary="Les documents"
-        secondary="Comment ça fonctionne"
-      />
-      {open ? <ExpandLess /> : <ExpandMore />}
-    </ListItem>
-    <Collapse in={open} timeout="auto" unmountOnExit>
+    <div>
+      <ListItem button component="a" href="/">
+        <ListItemIcon>
+          <HomeIcon />
+        </ListItemIcon>
+        <ListItemText primary="Accueil" />
+      </ListItem>
+      <ListItem button component="a" href="/espace">
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Mon espace" />
+      </ListItem>
+      <ListItem button component="a" href="/user-guide">
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Mode d'emploi" />
+      </ListItem>
+      <ListItem button onClick={handleClick}>
+        <ListItemIcon>
+          <LibraryBooksIcon />
+        </ListItemIcon>
+        <ListItemText
+          primary="Les documents"
+          secondary="Comment ça fonctionne"
+        />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
-          <ListItem button component="a" href="/nac-python-gui"  className={classes.nested}>
+          <ListItem
+            button
+            component="a"
+            href="/nac-python-gui"
+            className={classes.nested}
+          >
             <ListItemIcon>
               <DesktopWindowsIcon />
             </ListItemIcon>
-            <ListItemText primary="NAC Desktop App"/>
+            <ListItemText primary="NAC Desktop App" />
           </ListItem>
-          <ListItem button component="a" href="/nac-react-app" className={classes.nested}>
+          <ListItem
+            button
+            component="a"
+            href="/nac-react-app"
+            className={classes.nested}
+          >
             <ListItemIcon>
               <PhoneIphoneIcon />
             </ListItemIcon>
             <ListItemText primary="NAC Web App" />
           </ListItem>
-          <ListItem button component="a" href="/train-data" className={classes.nested}>
+          <ListItem
+            button
+            component="a"
+            href="/train-data"
+            className={classes.nested}
+          >
             <ListItemIcon>
               <PetsIcon />
             </ListItemIcon>
@@ -83,8 +99,14 @@ export default function MainListItems() {
           </ListItem>
         </List>
       </Collapse>
-  </div>
-  )
-};
+      <ListItem button component="a" href="/about">
+        <ListItemIcon>
+          <InfoIcon />
+        </ListItemIcon>
+        <ListItemText primary="À propos" />
+      </ListItem>
+    </div>
+  );
+}
 
 export const secondaryListItems = <div></div>;
